4b36eeabbde3b35b96e505ff27f2da1ca263fee3
orochimeii <babymeyfong@gmail.com>
2022-04-12 16:09:17 -0300

    initial commit

---
+++ TP2_Grupo12/src/main/java/ar/edu/unju/fi/pvisual/aplicacion/ejemplos/Tp2Grupo12Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pvisual.aplicacion.ejemplos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Grupo12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Grupo12Application.class, args);
+	}
+
+}
---
+++ TP2_Grupo12/src/test/java/ar/edu/unju/fi/pvisual/aplicacion/ejemplos/Tp2Grupo12ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pvisual.aplicacion.ejemplos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2Grupo12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
