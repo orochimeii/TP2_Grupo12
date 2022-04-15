94892001807725c73be8f379036b2c6a140c571b
prueba <prueba@Fabricacion7>
2022-04-14 23:41:17 -0300

    ejercicio 11

---
+++ TP2_Grupo12/src/main/java/ar/edu/unju/fi/ejercicio11/Principal11.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio11;
+
+		public class Principal11 {
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				int i=320;
+				do{
+					System.out.println(i);
+					i=i-20;
+				} while (i >= 160); 
+
+			}
+
+}
